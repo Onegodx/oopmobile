5825fc6c0b733f569a786c03048af2fb904c2b4c
Onegodx <113604166+Onegodx@users.noreply.github.com>
2024-10-24 00:57:30 +0200

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,18 @@
 class TimeConverter(private var minutes: Int, private var seconds: Int) {
 
-    // Метод для формирования строки с информацией об объекте
+    //информацияй об объекте метод
     fun info(): String {
         return "Минуты: $minutes, Секунды: $seconds"
     }
 
-    // Метод для вычисления общего количества секунд
+    // вычисления общего количества секунд
     fun totalSeconds(): Int {
         return minutes * 60 + seconds
     }
 }
 
 fun main() {
-    // Ввод значений полей
+  
     println("Введите количество минут:")
     val minutes = readLine()?.toIntOrNull() ?: 0
 
@@ -22,10 +22,10 @@ fun main() {
     // Создание объекта
     val timeConverter = TimeConverter(minutes, seconds)
 
-    // Вывод информации об объекте
+    
     println(timeConverter.info())
 
-    // Вычисление общего количества секунд
+
     val total = timeConverter.totalSeconds()
     println("Общее количество секунд: $total")
 }
